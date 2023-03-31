ec0e0d20e6828f31fcc164259eedc1d55bdc8852
jakefull <jakefuller@live.com>
2023-03-30 19:29:26 -0600

    Began implementing login but page stuck

--- typemory-html/database.js
+++ typemory-html/database.js
@@ -1,4 +1,6 @@
 const {MongoClient} = require('mongodb');
+const uuid = require('uuid');
+const bcrypt = require('bcrypt');
 
 const userName = process.env.MONGOUSER;
 const password = process.env.MONGOPASSWORD;
@@ -9,10 +11,10 @@ if (!userName) {
 }
 
 const url = `mongodb+srv://${userName}:${password}@${hostname}`;
-console.log(url);
 
 const client = new MongoClient(url);
 const scoreCollection = client.db('typemory').collection('score');
+const userCollection = client.db('typemory').collection('user');
 
 function addScore(score) {
   scoreCollection.insertOne(score);
@@ -27,5 +29,27 @@ function getHighScores() {
   const cursor = scoreCollection.find(query, options);
   return cursor.toArray();
 }
+//BEGIN USER FXNS
+function getUser(email) {
+  return userCollection.findOne({ email: email });
+}
 
-module.exports = {addScore, getHighScores}; //add any new fxns here
+function getUserByToken(token) {
+  return userCollection.findOne({ token: token });
+}
+
+async function createUser(email, password) {
+  // Hash the password before we insert it into the database
+  const passwordHash = await bcrypt.hash(password, 10);
+
+  const user = {
+    email: email,
+    password: passwordHash,
+    token: uuid.v4(), //what does uuid do?
+  };
+  await userCollection.insertOne(user);
+
+  return user;
+}
+
+module.exports = {addScore, getHighScores, getUser, getUserByToken, createUser}; //add any new fxns here
--- typemory-html/public/login.js
+++ typemory-html/public/login.js
@@ -1,6 +1,84 @@
-function login() {
-    const nameEl = document.querySelector("#name");
-    localStorage.setItem("userName", nameEl.value);
-    window.location.href = "play.html";
+// function login() {
+//     const nameEl = document.querySelector("#name");
+//     localStorage.setItem("userName", nameEl.value);
+//     window.location.href = "play.html"; //shouldn't this be type.html?
+//   }
+(async () => { //this fxn is called right away
+  let authenticated = false;
+  const userName = localStorage.getItem('userName');
+  if (userName) {
+    const nameEl = document.querySelector('#userName');
+    nameEl.value = userName;
+    const user = await getUser(nameEl.value);
+    authenticated = user?.authenticated;
   }
-  
+
+  if (authenticated) {
+    document.querySelector('#playerName').textContent = userName; //see index.html
+    setDisplay('loginControls', 'none');
+    setDisplay('playControls', 'block');
+  } else {
+    setDisplay('loginControls', 'block');
+    setDisplay('playControls', 'none');
+  }
+})();
+
+async function loginUser() {
+  loginOrCreate(`/api/auth/login`);
+}
+
+async function createUser() {
+  loginOrCreate(`/api/auth/create`);
+}
+
+async function loginOrCreate(endpoint) {
+  const userName = document.querySelector('#userName')?.value;
+  const password = document.querySelector('#password')?.value;
+  const response = await fetch(endpoint, {
+    method: 'post',
+    body: JSON.stringify({ email: userName, password: password }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  });
+  const body = await response.json();
+  //delete this
+  console.log(response);
+
+  if (response?.status === 200) {
+    localStorage.setItem('userName', userName);
+    window.location.href = 'scores.html'; //THIS ISN'T BEING CALLED!
+  } else {
+    const modalEl = document.querySelector('#errorBox');
+    modalEl.querySelector('.modal-body').textContent = `⚠ Error: ${body.msg}`;
+    const errorBox = new bootstrap.Modal(modalEl, {});
+    errorBox.show();
+  }
+}
+
+function play() {
+  window.location.href = 'type.html';
+}
+
+function logout() {
+  fetch(`/api/auth/logout`, {
+    method: 'delete',
+  }).then(() => (window.location.href = '/'));
+}
+
+async function getUser(email) {
+  let scores = [];
+  const response = await fetch(`/api/user/${email}`);
+  if (response.status === 200) {
+    return response.json();
+  }
+
+  return null;
+}
+
+function setDisplay(controlId, display) { //what does this do?
+  const playControlEl = document.querySelector(`#${controlId}`);
+  if (playControlEl) {
+    playControlEl.style.display = display;
+  }
+}
